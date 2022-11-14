7bddcbfd1ed612df39eb51241917f2144ae95e29
SohilNadarajan <32506686+SohilNadarajan@users.noreply.github.com>
2022-11-13 16:27:14 -0800

    explore completed

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -45,15 +45,26 @@ function initializeServiceWorker() {
   // We first must register our ServiceWorker here before any of the code in
   // sw.js is executed.
   // B1. TODO - Check if 'serviceWorker' is supported in the current browser
+  if ('serviceWorker' in navigator) {
   // B2. TODO - Listen for the 'load' event on the window object.
-  // Steps B3-B6 will be *inside* the event listener's function created in B2
-  // B3. TODO - Register './sw.js' as a service worker (The MDN article
-  //            "Using Service Workers" will help you here)
-  // B4. TODO - Once the service worker has been successfully registered, console
-  //            log that it was successful.
-  // B5. TODO - In the event that the service worker registration fails, console
-  //            log that it has failed.
-  // STEPS B6 ONWARDS WILL BE IN /sw.js
+    window.addEventListener('load', () => {
+      // Steps B3-B6 will be *inside* the event listener's function created in B2
+      // B3. TODO - Register './sw.js' as a service worker (The MDN article
+      //            "Using Service Workers" will help you here)
+      navigator.serviceWorker.register('/Lab7_Starter/sw.js').then((reg) => {
+      // B4. TODO - Once the service worker has been successfully registered, console
+      //            log that it was successful.
+        console.log('Service worker has been successfully registered:', reg);
+      }, (e) => {
+      // B5. TODO - In the event that the service worker registration fails, console
+      //            log that it has failed.
+      console.error(`Service worker has failed registration: ${e}`);
+      // STEPS B6 ONWARDS WILL BE IN /sw.js
+      });
+    });
+  } else {
+    console.error('Service workers are not supported in this browser');
+  }
 }
 
 /**
